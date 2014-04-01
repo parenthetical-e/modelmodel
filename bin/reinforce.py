74bdf26bc99e7713c02224adabf3d0797722216f
Erik Peterson <Erik.Exists@gmail.com>
2014-04-01 10:55:21 -0500

    Added bin to generate RL data tables from the CL

---
+++ bin/reinforce.py
@@ -0,0 +1,72 @@
+"""Create a table of simulated RL (Rescorla Wagner) data"""
+import os
+import argparse
+import pandas as pd
+import numpy as np
+from modelmodel.behave import behave
+from modelmodel.behave import reinforce
+
+parser = argparse.ArgumentParser(
+        description="Create a table of simulated RL (Rescorla Wagner) data",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        )
+parser.add_argument(
+        "name", type=str,
+        help="Name of table"
+        )
+parser.add_argument(
+        "N", type=int,
+        help="Number of samples"
+        )
+parser.add_argument(
+        "--behave", type=str, default='learn',
+        help="Behavior learning mode"
+        )
+parser.add_argument(
+        "--n_cond", type=int, default=1,
+        help="N cond"
+        )
+parser.add_argument(
+        "--n_trials", type=int, default=60,
+        help="N trials/cond"
+        )
+parser.add_argument(
+        "--alpha",
+        type=float, nargs="+", default=None,
+        help="Set alpha values"
+        )
+parser.add_argument(
+        "--seed",
+        default=42, type=int,
+        help="RandomState seed"
+        )
+args = parser.parse_args()
+prng = np.random.RandomState(args.seed)
+
+dfs = []
+for n in range(args.N):
+    if args.behave == 'learn':
+        trial, acc, p, prng = behave.learn(
+                args.n_cond, args.n_trials, 
+                loc=prng.normal(3, .3), prng=prng
+                )
+    elif args.behave == 'random':
+        trial, acc, p, prng = behave.random(
+                args.n_cond, args.n_trials, prng=prng
+                )
+    else:
+        raise ValueError('--behave not understood')
+    
+    df, rlpars = reinforce.rescorla_wagner(
+            trial, acc, p, alpha=args.alpha, prng=prng
+            )
+    del df['rand']
+    
+    l = trial.shape[0]
+    df['count'] = np.repeat(n, l)
+    df['index'] = np.arange(l, dtype=np.int)
+    
+    dfs.append(df)
+    
+df = pd.concat(dfs, axis=0)
+df.to_csv(args.name, index=False, float_format='%.8f')
